ecea4702121b9fed4a07251e9be3e0cc3fb66807
cheng-pop <70852012+cheng-pop@users.noreply.github.com>
2021-04-06 14:27:21 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    hi
+  </div>
+  <Cheng />
+</template>
+
+<script>
+import Cheng from './components/Cheng.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Cheng
+  }
+}
+</script>

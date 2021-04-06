f67e60eb40e138b72644f78e6decb11e72d4dbb5
cheng-pop <70852012+cheng-pop@users.noreply.github.com>
2021-04-06 16:27:48 +0800

    调节Topnav和aside的位置

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -32,3 +32,33 @@ export default {
     }
 }
 </script>
+<style lang="scss" scoped>
+    $aside-index : 10;
+    aside {
+        background: lightblue;
+        width: 150px;
+        padding: 16px 0;
+        position: fixed;
+        top: 0;
+        left: 0;
+        padding-top: 70px;
+        height: 100%;
+        z-index: $aside-index;
+        >h2 {
+          margin-bottom: 4px;
+          padding: 0 16px;
+        }
+        >ol {
+          >li {
+            >a {
+              display: block;
+              padding: 4px 16px;
+              text-decoration: none;
+            }
+            .router-link-active {
+              background: white;
+            }
+          }
+        }
+    }
+</style>

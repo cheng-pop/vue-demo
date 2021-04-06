1fb210f46aa12e0dd05da0c5cb02eae7738e8c08
cheng-pop <70852012+cheng-pop@users.noreply.github.com>
2021-04-06 16:13:55 +0800

    优化Home页面

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -20,4 +20,29 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+$green: #02bcb0;
+$border-radius: 4px;
+$color: #007974;
+.banner {
+  color: $color;
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  >.actions {
+    padding: 8px 0;
+    a {
+      margin: 0 8px;
+      background: $green;
+      color: white;
+      display: inline-block;
+      padding: 8px 24px;
+      border-radius: $border-radius;
+      &:hover {
+        text-decoration: none;
+      }
+    }
+  }
+}
 </style>

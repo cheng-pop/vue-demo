48b81218e15b53bf51d99b8d453d4234a265b4d3
cheng-pop <70852012+cheng-pop@users.noreply.github.com>
2021-04-08 16:03:09 +0800

    button默认绑定

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,17 @@
 <template>
-     <button>
-        <slot />
-      </button>
+    <div :size="size">
+        <!-- $artts获取所有属性 -->
+        <button v-bind="rest">
+            <slot />
+        </button>
+    </div>
 </template>
+<script lang="ts">
+export default {
+   inheritAttrs:false ,  //继承属性关闭 (默认绑定)
+   setup(props,context) {
+       const {size,...rest} = context.attrs
+       return {size,rest}
+   }
+}
+</script>

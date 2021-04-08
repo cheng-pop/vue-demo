fb6cecd3a23565f54ddf0420e601fa3209e3d222
cheng-pop <70852012+cheng-pop@users.noreply.github.com>
2021-04-09 02:28:06 +0800

    Dialog显示

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,15 +1,25 @@
 <template>
     <div>
-        <Dialog></Dialog>
+        <Button @click="toggle">toggle</Button>
+        <Dialog :visible="x"></Dialog>
     </div>
 </template> 
-<script>
+<script lang="ts">
 import Dialog from '../lib/Dialog.vue'
+import Button from '../lib/Button.vue'
+import { ref } from 'vue'
 export default {
     components:{
-        Dialog
+        Dialog,
+        Button
+    },
+    setup(){
+        const x = ref(false)
+        const toggle = () =>{
+           x.value = !x.value 
+        }
+        return {x,toggle}
     }
-    
 }
 </script>
 <style lang="scss" scoped>

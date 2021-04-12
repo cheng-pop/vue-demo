89e8c9a484527d2d70c133219a87a82ff432ba54
cheng-pop <70852012+cheng-pop@users.noreply.github.com>
2021-04-12 22:17:19 +0800

    markdown

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { router } from './router';
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.scss'
+import 'github-markdown-css'
 
 
 const app = createApp(App)
--- src/views/GetStart.vue
+++ src/views/GetStart.vue
@@ -1,5 +1,15 @@
 <template>
-    <div>
-        getstart
-    </div>
+    <article class="markdown-body" v-html="md">
+    </article>
 </template>
+<script>
+import md from '../markdown/get-start.md'
+export default {
+    data() {
+        return {
+            md
+        }
+    },
+    
+}
+</script>
